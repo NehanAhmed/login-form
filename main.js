a7a34070baff82fdcf90d1bf75b95b61273c04f2
Nehan Ahmed <nehanahmed2k23@gmail.com>
2024-07-04 12:59:10 +0500

    changes

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-function darkMode(){
-    var element = document.body;
+function validate(){
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+    var usernameerr = document.getElementById("username-error")
+    var passworderr = document.getElementById("password-error")
 
-    element.classList.toggle("dark-mode")
+    if(username=="" ){
+        usernameerr.innerHTML = "Please fill the name field!";
+        // return false;
+       
+    }
+    else if(password==""){
+        passworderr.innerHTML = "Please fill the password field!";
+        
+        if(password.length < 10){
+            usernameerr.innerHTML = "Please Enter a Strong Password"
+        }
+        else{
+            alert("Submitted")
+        }
+    }
+
 }
